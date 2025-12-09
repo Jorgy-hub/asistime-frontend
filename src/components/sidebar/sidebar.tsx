095c66b2974f406344eba2a2037e937d956ced91
jorgypzk <jorgeadolfog79@gmail.com>
2025-12-09 16:48:12 -0600

    chore: icons

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -81,16 +81,7 @@ const Sidebar = ({ admin = false }: SidebarProps) => {
     <div className="w-24 h-screen text-white px-4 pt-6 pb-4 bg-zinc-950 flex flex-col">
       {/* Logo */}
       <div className="-mx-4 px-4 flex justify-center pb-4 mb-4 shrink-0 border-zinc-700">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-12 w-12 text-white">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 4.875c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 
-                    .621-.504 1.125-1.125 1.125h-4.5A1.125 1.125 0 0 1 3.75 9.375v-4.5ZM3.75 14.625c0-.621.504-1.125 
-                    1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5a1.125 1.125 
-                    0 0 1-1.125-1.125v-4.5ZM13.5 4.875c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 
-                    1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5A1.125 1.125 0 0 1 13.5 9.375v-4.5Z" />
-          <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 6.75h.75v.75h-.75v-.75ZM6.75 16.5h.75v.75h-.75v-.75ZM16.5 6.75h.75v.75h-.75v-.75ZM13.5 
-                    13.5h.75v.75h-.75v-.75ZM13.5 19.5h.75v.75h-.75v-.75ZM19.5 13.5h.75v.75h-.75v-.75ZM19.5 19.5h.75v.75h-.75v-.75ZM16.5 
-                    16.5h.75v.75h-.75v-.75Z" />
-        </svg>
+        <img src="/images/logo.png" alt="Logo" className="w-12 h-12 object-contain" />
       </div>
 
       {/* Centered nav */}
